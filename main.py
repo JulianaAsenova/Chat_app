73ebcc7333a8d070ea54049ff8e787a66122bb63
JulianaAsenova <jasenova07@gmail.com>
2020-03-18 13:26:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def session():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
