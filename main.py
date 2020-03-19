c630732520ed7057c0e6837fe1a70b25c7f62b51
JulianaAsenova <jasenova07@gmail.com>
2020-03-19 12:50:33 +0200

    Implement receiving and sending a message

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ socketio = SocketIO(app)
 @app.route('/')
 def session():
     return render_template('index.html')
+    
+def ask():
+    print'The message was received.'
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+	print('received json: ' + str(json))
+	socketio.emit('my response', json, callback=ask)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
